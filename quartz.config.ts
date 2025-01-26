299a9cf01ffd636e5c0539ed08b8284d939b86d6
alexander-dast <alexander-dast@pm.me>
2025-01-26 12:55:55 +0000

    Updating README and general config. Bump ./content

--- quartz.config.ts
+++ quartz.config.ts
@@ -70,7 +70,8 @@ const config: QuartzConfig = {
       Plugin.ObsidianFlavoredMarkdown({ enableInHtmlEmbed: false }),
       Plugin.GitHubFlavoredMarkdown(),
       Plugin.TableOfContents(),
-      Plugin.CrawlLinks({ markdownLinkResolution: "shortest" }),
+      Plugin.CrawlLinks({
+        markdownLinkResolution: "shortest"}),
       Plugin.Description(),
       Plugin.Latex({ renderEngine: "katex" }),
     ],
